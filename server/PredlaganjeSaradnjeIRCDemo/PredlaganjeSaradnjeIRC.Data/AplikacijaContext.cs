d06fcf7666e1eb348d2a03aee19e4e42ac2ad17d
Luka Radovanovic <lukadlm97@gmail.com>
2020-03-27 21:19:04 +0100

    Instalirani potrebni paketi za perzistenciju podataka sa SQL bazom podataka

---
+++ server/PredlaganjeSaradnjeIRCDemo/PredlaganjeSaradnjeIRC.Data/AplikacijaContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace PredlaganjeSaradnjeIRC.Data
+{
+    public class AplikacijaContext:DbContext
+    {
+        public AplikacijaContext()
+        {
+
+        }
+
+        public AplikacijaContext(DbContextOptions options):base(options)
+        {
+        }
+
+
+    }
+}
--- server/PredlaganjeSaradnjeIRCDemo/PredlaganjeSaradnjeIRC/Startup.cs
+++ server/PredlaganjeSaradnjeIRCDemo/PredlaganjeSaradnjeIRC/Startup.cs
@@ -26,6 +26,9 @@ namespace PredlaganjeSaradnjeIRC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSingleton(Configuration);
+
+       
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
