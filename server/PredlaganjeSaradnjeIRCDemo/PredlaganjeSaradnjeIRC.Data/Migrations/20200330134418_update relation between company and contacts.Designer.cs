97dbbdbdd4e953cc1360b77a11bdcf2bf65bc41f
Luka Radovanovic <lukadlm97@gmail.com>
2020-03-30 16:20:07 +0200

    fixed db structure from model

--- server/PredlaganjeSaradnjeIRCDemo/PredlaganjeSaradnjeIRC.Data/Migrations/20200330134418_update relation between company and contacts.Designer.cs
+++ server/PredlaganjeSaradnjeIRCDemo/PredlaganjeSaradnjeIRC.Data/Migrations/20200330134418_update relation between company and contacts.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PredlaganjeSaradnjeIRC.Data;
 
 namespace PredlaganjeSaradnjeIRC.Data.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20200330134418_update relation between company and contacts")]
+    partial class updaterelationbetweencompanyandcontacts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
