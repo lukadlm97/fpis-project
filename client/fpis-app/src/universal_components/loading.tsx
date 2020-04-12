6263cb7e496d73fda10eb74038396e329a2bb913
Luka Radovanovic <lukadlm97@gmail.com>
2020-04-12 23:20:24 +0200

    implemented display for requests

---
+++ client/fpis-app/src/universal_components/loading.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import loadingGif from '../image/gif/loading-arrow.gif'
+
+function Loading(){
+    return(
+        <>
+        <div className="loading">
+            <h4>rooms data loading... </h4>
+            <img src={loadingGif} alt=""/>
+        </div>
+        </>
+    )
+}
+
+export default Loading
